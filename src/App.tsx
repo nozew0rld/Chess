222a3a90baaee6fbfaef1a27f8ee5a30b2d599e5
Bataa <obito6564@gmail.com>
2026-05-22 22:59:54 +0200

    chessBoard done

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="">
       <Chessboard />
-      tooo lalar
     </div>
   );
 }
--- src/components/Chessboard.tsx
+++ src/components/Chessboard.tsx
@@ -1,23 +1,28 @@
 import React from "react";
 
-const verticalAxis = [1, 2, 3, 4, 5, 6, 7, 8];
+const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const horizontalAxis = ["a", "b", "c", "d", "d", "f", "g", "h"];
 function Chessboard() {
   let board = [];
 
-  for (let i = 0; i < horizontalAxis.length; i++) {
-    for (let j = 0; j < verticalAxis.length; j++) {
-      (horizontalAxis[i], verticalAxis[j]);
-      board.push(
-        <span className="text-black">
-          {horizontalAxis[i]} {verticalAxis[j]}
-        </span>,
-      );
+  for (let j = verticalAxis.length - 1; j >= 0; j--) {
+    for (let i = 0; i < horizontalAxis.length; i++) {
+      const number = j + i + 2;
+
+      if (number % 2 === 0) {
+        (horizontalAxis[i], verticalAxis[j]);
+        board.push(<div className="bg-[#ba5645] w-[100px] h-[100px]"></div>);
+      } else {
+        (horizontalAxis[i], verticalAxis[j]);
+        board.push(<div className="bg-[#f5dcc4] w-[100px] h-[100px]"></div>);
+      }
     }
   }
   return (
     <div className="flex justify-center items-center h-screen">
-      <div className="w-[801px] h-[801px] bg-amber-200">{board}</div>
+      <div className="w-[800px] h-[800px] bg-amber-200 grid grid-cols-8 grid-rows-8">
+        {board}
+      </div>
     </div>
   );
 }
