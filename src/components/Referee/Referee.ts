30f68488b31e08b8aa482a6352e63d606b10c6ff
Bataa <obito6564@gmail.com>
2026-06-04 19:23:07 +0200

    knight movement

--- src/components/Referee/Referee.ts
+++ src/components/Referee/Referee.ts
@@ -1,15 +1,25 @@
 import {
   PieceType,
+  samePosition,
   TeamType,
   type Piece,
   type Position,
 } from "../../models/Piece";
 
 export default class Referee {
-  tileIsOccupied(x: number, y: number, boardState: Piece[]): boolean {
-    const piece = boardState.find(
-      (p) => p.position.x === x && p.position.y === y,
+  tileIsEmpyOrOccupiedByOpponent(
+    position: Position,
+    boardState: Piece[],
+    team: TeamType,
+  ) {
+    return (
+      !this.tileIsOccupied(position, boardState) ||
+      this.tileIsOccupiedByOpponent(position, boardState, team)
     );
+  }
+
+  tileIsOccupied(position: Position, boardState: Piece[]): boolean {
+    const piece = boardState.find((p) => samePosition(p.position, position));
     if (piece) {
       return true;
     } else {
@@ -17,13 +27,12 @@ export default class Referee {
     }
   }
   tileIsOccupiedByOpponent(
-    x: number,
-    y: number,
+    position: Position,
     boardState: Piece[],
     team: TeamType,
   ): boolean {
     const piece = boardState.find(
-      (p) => p.position.x === x && p.position.y === y && p.team !== team,
+      (p) => samePosition(p.position, position) && p.team !== team,
     );
     if (piece) {
       return true;
@@ -79,14 +88,9 @@ export default class Referee {
         desiredPostion.y - initialPosition.y === 2 * pawnDirection
       ) {
         if (
+          !this.tileIsOccupied(desiredPostion, boardState) &&
           !this.tileIsOccupied(
-            desiredPostion.x,
-            desiredPostion.y,
-            boardState,
-          ) &&
-          !this.tileIsOccupied(
-            desiredPostion.x,
-            desiredPostion.y - pawnDirection,
+            { x: desiredPostion.x, y: desiredPostion.y - pawnDirection },
             boardState,
           )
         ) {
@@ -96,9 +100,7 @@ export default class Referee {
         initialPosition.x === desiredPostion.x &&
         desiredPostion.y - initialPosition.y === pawnDirection
       ) {
-        if (
-          !this.tileIsOccupied(desiredPostion.x, desiredPostion.y, boardState)
-        ) {
+        if (!this.tileIsOccupied(desiredPostion, boardState)) {
           return true;
         }
       }
@@ -109,14 +111,7 @@ export default class Referee {
       ) {
         //attack in upper or bottom left corner
         console.log("upper / bottom left");
-        if (
-          this.tileIsOccupiedByOpponent(
-            desiredPostion.x,
-            desiredPostion.y,
-            boardState,
-            team,
-          )
-        ) {
+        if (this.tileIsOccupiedByOpponent(desiredPostion, boardState, team)) {
           return true;
         }
       } else if (
@@ -125,15 +120,42 @@ export default class Referee {
       ) {
         //attack in upper of bottom right corner
         console.log("upper / bottom right");
-        if (
-          this.tileIsOccupiedByOpponent(
-            desiredPostion.x,
-            desiredPostion.y,
-            boardState,
-            team,
-          )
-        ) {
+        if (this.tileIsOccupiedByOpponent(desiredPostion, boardState, team)) {
           return true;
+        }
+      }
+    } else if (type === PieceType.KNIGHT) {
+      //moving logic for the knight
+      for (let i = -1; i < 2; i += 2) {
+        for (let j = -1; j < 2; j += 2) {
+          //top and bottom side
+          if (desiredPostion.y - initialPosition.y === 2 * i) {
+            if (desiredPostion.x - initialPosition.x === j) {
+              if (
+                this.tileIsEmpyOrOccupiedByOpponent(
+                  desiredPostion,
+                  boardState,
+                  team,
+                )
+              ) {
+                return true;
+              }
+            }
+          }
+          // right and left side
+          if (desiredPostion.x - initialPosition.x === 2 * i) {
+            if (desiredPostion.y - initialPosition.y === j) {
+              if (
+                this.tileIsEmpyOrOccupiedByOpponent(
+                  desiredPostion,
+                  boardState,
+                  team,
+                )
+              ) {
+                return true;
+              }
+            }
+          }
         }
       }
     }
